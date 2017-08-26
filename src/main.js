7bfc37dae0d4e34fc3c087672eae6021ce067453
CJ <cjzhang88@gmail.com>
2017-08-26 11:36:38 +0800

    价格排序和分页

--- src/main.js
+++ src/main.js
@@ -4,17 +4,19 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueLazyLoad from 'vue-lazyload'
+import infiniteScroll from 'vue-infinite-scroll'
 
 Vue.config.productionTip = false
 
 Vue.use(VueLazyLoad, {
-  loading: "/static/loading-svg/loading-bars.svg"
+    loading: "/static/loading-svg/loading-bars.svg"
 })
+Vue.use(infiniteScroll)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
 })
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -9,7 +9,7 @@
           <div class="filter-nav">
             <span class="sortby">Sort by:</span>
             <a href="javascript:void(0)" class="default cur">Default</a>
-            <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+            <a href="javascript:void(0)" @click="sortGoods"  class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
             <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPopShow">Filter by</a>
           </div>
           <div class="accessory-result">
@@ -34,13 +34,16 @@
                     </div>
                     <div class="main">
                       <div class="name">{{ item.prodcutName }}</div>
-                      <div class="price">{{ item.productPrice }}</div>
+                      <div class="price">{{ item.salePrice }}</div>
                       <div class="btn-area">
                         <a href="javascript:;" class="btn btn--m">加入购物车</a>
                       </div>
                     </div>
                   </li>
                 </ul>
+                <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
+                  加载中...
+                </div>
               </div>
             </div>
           </div>
@@ -63,6 +66,10 @@
     data() {
       return {
         goodsList:[],
+        sortFlag: true,
+        page: 1,
+        pageSize:8,
+        busy:false,
         priceFilter: [
           {
             startPrice: '0.00',
@@ -88,23 +95,39 @@
       NavBread
     },
     mounted() {
-      this.getGoodslist();
+      this.getGoodslist(false);
     },
     methods: {
-      getGoodslist(){
-          // axios.get("/goods").then((result) => {
-          //   var res = result.data;
-          //   this.goodsList = res.result;
-          // });
+      getGoodslist(flag){
+        let param = {
+          page:this.page,
+          pageSize:this.pageSize,
+          sort:this.sortFlag?1:-1
+        }
           axios({
             method:'get',
             url:'http://localhost:3000/goods',
             headers: {
               'Content-Type': 'application/x-www-form-urlencoded'
-            }
+            },
+            params:param
           }).then((result) => {
-            var res = result.data;
-            this.goodsList = res.result.list;
+            let res = result.data;
+            if(res.status == '0'){
+              if (flag) {
+                this.goodsList = this.goodsList.concat(res.result.list);
+                if (res.result.count == 0) {
+                  this.busy = true;
+                }else{
+                  this.busy = false;
+                }
+              }else{
+                this.goodsList = res.result.list;
+                this.busy = false;
+              }
+            }else{
+              this.goodsList = [];
+            }
           })
       },
       showFilterPopShow(){
@@ -112,12 +135,24 @@
         this.overLayFlag = true;
       },
       closePop(){
-         this.filterBy = false;
+        this.filterBy = false;
         this.overLayFlag = false;
       },
       setPriceFilter(index){
         this.priceChecked = index;
         this.closePop();
+      },
+      sortGoods(){
+        this.sortFlag = !this.sortFlag;
+        this.page = 1;
+        this.getGoodslist(false);
+      },
+      loadMore(){
+        this.busy = true;
+        setTimeout(() => {
+          this.page++;
+          this.getGoodslist(true);
+        }, 500);
       }
     }
   }
