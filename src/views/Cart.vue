52c50d68d9298d1893a5dccfb745cdbee8a528f5
CJ <cjzhang88@gmail.com>
2017-09-11 19:11:44 +0800

    购物车跳转到地址页面，以及跳转的按钮的可点击判断

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -122,7 +122,7 @@
                 Item total: <span class="total-price">{{ totalPrice | currency('$') }}</span>
               </div>
               <div class="btn-wrap">
-                <a class="btn btn--red">Checkout</a>
+                <a class="btn btn--red" v-bind:class="{ 'btn--dis':checkedCount == 0}" @click="checkout">Checkout</a>
               </div>
             </div>
           </div>
@@ -274,6 +274,13 @@ export default {
            console.log("update suc")
          }
       });
+    },
+    checkout(){
+      if(this.checkedCount > 0){
+        this.$router.push({
+          path:"/address"
+        });
+      }
     }
   }
 }
